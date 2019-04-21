c37131431cfc89fb92e6713d38b201adad3881f3
Tastel123 <244674264@qq.com>
2019-04-21 23:52:04 +0800

    map.js

---
+++ JS/mult/map.js
@@ -0,0 +1,50 @@
+// 对象 林 gaodeMap
+const googleMap = {
+    show() {
+        console.log('开始渲染google地图');
+    }
+}
+
+const baiduMap = {
+    // 为互换做准备
+    show() {
+        console.log('开始渲染百度地图');
+    }
+}
+
+const gaodeMap = {
+    show() {
+        console.log('开始渲染gaode地图');
+    }
+}
+const sosoMap = {
+    
+}
+/**
+ * 
+ * @param {object} map 
+ */
+const renderMap = (map) => {
+    // map 检测
+    // if(map.show && typeof map.show === 'function') {
+    //     map.show();
+    // }
+    if(map.show && map.show instanceof Function){
+        map.show();
+    }
+        
+    // googleMap.show();
+    // baiduMap.show();
+    // 分支太多
+    // if(type === 'baidu')
+    //     baiduMap.show();
+    // else if(type === 'google')
+    //     googleMap.show();
+    // else if(type === 'gaode')
+    //     gaodeMap.show();
+
+}
+
+renderMap(baiduMap);
+renderMap(googleMap);
+renderMap(sosoMap);
