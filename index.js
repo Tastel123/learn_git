97561b0010e05a2364bbf98e05a5737c43f36ea4
Tastel123 <42127819+Tastel123@users.noreply.github.com>
2019-06-07 22:15:58 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,45 +1,75 @@
-const puppeteer = require('puppeteer');
-const sleep = (time) => new Promise((resolve) => {
-    setTimeout(() => {
-        resolve(1);
-    }, time)
-})
-async function getLeetCode() {
-    const browser = await puppeteer.launch({
-      headless: false  
-    })
-    const page = await browser.newPage();
-    page.setViewport({
-        width: 1376,
-        height: 768
-    })
-    await page.goto('https://leetcode-cn.com/u/yan-bo-yi', {
-        waitUntil: 'networkidle0'
-    });
-    // 等待一秒
-    await sleep(3000);
-    const data = await page.evaluate(() => {
-        const rank = document
-        .querySelector('.css-1x529is-RankNumber')
-        console.log('这段字来自evaluate');
+const Koa = require('koa')
+const Router = require('koa-router')
+const koaSession = require('koa-session')
+const staticServer = require('koa-static')
+const path = require('path')
+const app = new Koa()
+const router = new Router()
+const session_signed_key = ['secret']
 
-        const submitHistory = document
-        .querySelectorAll('.css-i7v0bm-StackRow');
-        // 转为真正的数组
-        const submitHistoryArray = Array.from(submitHistory);
-        const submitHistoryList = submitHistoryArray
-        .map(submitNode => {
-            return submitNode.innerText;
-        })
-        return {
-            rank: rank.innerText,
-            submitHistoryList
-        }
-    })
-    console.log('rank data', data);
-    await page.screenshot({
-        path:'./yanboyi.png'
-    })
-    // browser.close();
+app.keys = session_signed_key
+const sessionConfig = {
+    key: 'sid',  // 放到 cookie 里面的name属性
+    maxAge: 5000
 }
-getLeetCode();
+app.use(koaSession(sessionConfig, app))
+
+app.use(
+    staticServer(path.join(__dirname, './static'))
+)
+
+// router.get('/', async (ctx) => {
+//     ctx.cookies.set('fromserver1','fromserver1',{
+//         maxAge: 5000
+//     })
+//     ctx.cookies.set('user','user',{
+//         path: '/user',
+//         httpOnly: false
+//     })
+//     ctx.cookies.set('user-abc','user-abc',{
+//         path: '/user/abc',
+//     })
+//     ctx.cookies.set('post','post',{
+//         path: '/post',
+//     })
+//     ctx.type = 'html'
+//     ctx.body = `
+//     <a href = "/user"> -> user </a>
+//     <a href = "/post"> -> post </a>
+//     `
+// })
+// router.get('/user', async (ctx) => {
+//     ctx.body = 'user page'
+// })
+// router.get('/post', async (ctx) => {
+//     ctx.body = 'post page'
+// })
+// router.get('/user/abc', async (ctx) => {
+//     ctx.body = '/user/abc page'
+// })
+router.get('/login', async (ctx) => {
+    const { name, password } = ctx.query
+    if (name === 'test' && password === 'test') {
+        // 写到 session
+        ctx.session.login = true
+        ctx.type = 'html'
+        ctx.body = '登录成功' + '<a href="/testlogin">测试登录</a>'
+    }
+    else {
+        ctx.session.login = false
+        ctx.body = '账户错误'
+    }
+})
+router.get('/testlogin', async (ctx) => {
+    if (ctx.session.login) {
+        ctx.body = '成功登录'
+    } else {
+        ctx.redirect('/')
+    }
+})
+app
+.use(router.routes())
+
+app.listen(7878, () => {
+    console.log('server is running 7878');
+})
